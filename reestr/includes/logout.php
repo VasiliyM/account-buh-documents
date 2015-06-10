dda1719a4571d4585ef467ccde1d2bd83e4d00fd
panches <panches@ukr.net>
2015-06-10 11:55:56 +0300

    отмена

--- reestr/includes/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-// закрыть сессию
-	session_start();
-	unset($_SESSION['session_username']);
-    unset($_SESSION['session_idusername']);
-	session_destroy();
-	header("location: ../index.html");
-?>
